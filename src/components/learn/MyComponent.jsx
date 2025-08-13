f4e0c20c41312f70995ca85828809e77668480fd
buivanthai <buithaild230899@gmail.com>
2025-08-13 13:51:30 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,21 @@
 
 import './style.css'
 const MyComponent = () => {
+    // const val = "Heyyy" // string
+    // const val = 23 // number
+    // const val = true // boolean
+    // const val = undefined
+    // const val = null
+    // const val = [1, 2, 3]
+    const val = {
+        name: "Thai",
+        age: 26
+    }
     return (
         <>
-            <div>Heyyyy</div>
+            {/* <div>{val.name} & You</div> */}
+            <div>{JSON.stringify(val)} & You</div>
+            <div>{console.log("Hello")}</div>
             <div className='child'
                 style={{ borderRadius: "10px" }}
             >Child</div>
