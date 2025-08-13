ec943045b34602d0bba176c3cb59c08fbc258e83
buivanthai <buithaild230899@gmail.com>
2025-08-13 16:04:02 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,11 +2,29 @@ const TodoNew = (props) => {
     console.log(">>> check props new add: ", props)
     const { addNewTodo } = props
     // addNewTodo('Thai')
+
+    const handleClick = () => {
+        alert('click me')
+    }
+
+    // const handleOnChange = (event) => {
+    //     console.log(">>> handleOnChange", event.target.value)
+    // }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name)
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
-        </div>
+            <input type="text"
+                // onChange={handleOnChange}
+                onChange={(event) => { handleOnChange(event.target.value) }}
+            />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            > Add</button >
+        </div >
     )
 }
 
