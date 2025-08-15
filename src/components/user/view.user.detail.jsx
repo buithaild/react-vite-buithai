d1d9175397bbce17ab484b5beb37398c6a413f01
buivanthai <buithaild230899@gmail.com>
2025-08-15 21:16:24 +0700

    video 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetail = (props) => {
 
@@ -10,6 +10,7 @@ const ViewUserDetail = (props) => {
     } = props
     return (
         <Drawer
+            width={"40vw"}
             title="Detail User"
             closable={{ 'aria-label': 'Close Button' }}
             onClose={() => {
@@ -21,9 +22,37 @@ const ViewUserDetail = (props) => {
             {dataDetail ?
                 <>
                     <p>Id: {dataDetail._id}</p>
+                    <br />
                     <p>Full nmae: {dataDetail.fullName}</p>
+                    <br />
                     <p>Email: {dataDetail.email}</p>
+                    <br />
                     <p>Phone Number: {dataDetail.phone}</p>
+                    <br />
+                    <p>Avatar:</p>
+                    <br />
+                    <div>
+                        <img height={100} width={150} src={`${import.meta.env.VITE_BACKEND_URL
+                            }/images/avatar/${dataDetail.avatar}`} />
+                    </div>
+                    <div>
+                        <label htmlFor="btnUpload"
+                            style={{
+                                display: "block",
+                                width: "fit-content",
+                                marginTop: '15px',
+                                padding: "5px 10px",
+                                background: "orange",
+                                borderRadius: "5px",
+                                cursor: "pointer"
+                            }}
+                        >
+                            Upload avatar
+                        </label>
+                        <input type="file" hidden id='btnUpload' />
+                    </div>
+
+
                 </>
                 :
                 <><p>Khong co du lieu</p></>
